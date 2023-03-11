9783c5f86edb02c1ca7a6d148d62f1a1027f476b
Shiva Eslami <shivaslami@gmail.com>
2023-03-11 21:51:48 +0330

    refactor: move welcome content to a separate component

---
+++ components/WelcomeContent.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import weatherIcon from "@/public/weather-icon.png";
+
+const WelcomeContent = () => {
+  return (
+    <div className="flex flex-col gap-2 items-center">
+      <Image className="app-logo" src={weatherIcon} alt="weather icon" />
+      <h3 className="text-2xl">Welcome!</h3>
+    </div>
+  );
+};
+
+export default WelcomeContent;
