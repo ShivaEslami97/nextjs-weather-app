cc7c05b985303bdfdb16cdf7a4df3ba82425eff4
Shiva Eslami <shivaslami@gmail.com>
2023-03-11 21:35:20 +0330

    feat: add isLogin state for authentication

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,9 +1,15 @@
 import Image from "next/image";
 import emailIcon from "@/public/email_envelope_mail_send_icon.svg";
 import lockIcon from "@/public/lock_locker_icon.svg";
+import { useState } from "react";
 import InputContainer from "./InputContainer";
 
 const AuthForm = () => {
+  const [isLogin, setIslogin] = useState(true);
+  const switchAuthModeHandler = () => {
+    setIslogin((prevIsLogin) => !prevIsLogin);
+  };
+
   return (
     <form className="auth__form">
       <InputContainer
@@ -18,12 +24,26 @@ const AuthForm = () => {
         iconAlt="lock icon"
         iconsrc={lockIcon}
       />
-      <button className="auth__btn">Login</button>
+      {!isLogin && (
+        <InputContainer
+          type="password"
+          placeholder="Confirm your password"
+          iconAlt="lock icon"
+          iconsrc={lockIcon}
+        />
+      )}
+      <button className="auth__btn">
+        {isLogin ? "Login" : "Create Account"}
+      </button>
       <div className="auth__mode">
-        Dont you have an account?
-        <a className="text-primary ml-2" href="#">
-          Sign Up
-        </a>
+        {isLogin ? "Dont you have an account?" : "Login with existing account"}
+        <button
+          type="button"
+          className="text-primary ml-2"
+          onClick={switchAuthModeHandler}
+        >
+          {isLogin ? "Sign Up" : "Sign In"}
+        </button>
       </div>
     </form>
   );
