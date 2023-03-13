16917a79398f9aa45cca6d118712e832df6abf33
Shiva Eslami <shivaslami@gmail.com>
2023-03-13 17:31:31 +0330

    feat: Add priority to Image component

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,35 @@ import AuthForm from "@/components/AuthForm";
 import Layout from "@/components/Layout";
 import Image from "next/image";
 import weatherIcon from "@/public/weather-icon.png";
+import { useEffect } from "react";
+import { getSession, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 
 export default function Home() {
+  const { status, data } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    getSession().then((session) => {
+      if (session) {
+        router.replace("/weather");
+      } else {
+        console.log("false");
+      }
+    });
+    console.log(status);
+  }, [router, status]);
+
   return (
     <>
       <Layout title="weather App login page" className="w-1/2">
         <div className="flex flex-col gap-2 items-center">
-          <Image className="app-logo" src={weatherIcon} alt="weather icon" />
+          <Image
+            className="app-logo"
+            priority
+            src={weatherIcon}
+            alt="weather icon"
+          />
           <h3 className="text-2xl">Welcome!</h3>
         </div>
         <AuthForm />
