4ae9f03be08f810d40efdd48f28aed25bb225a41
Shiva Eslami <shivaslami@gmail.com>
2023-03-13 15:37:29 +0330

    Add unauthenticated page for protected route

---
+++ pages/unauthenticated.tsx
@@ -0,0 +1,11 @@
+import Layout from "@/components/Layout";
+
+const Unauthenticated = () => {
+  return (
+    <Layout title="authenticated page">
+      <h1 className="text-lg">You should login to view this page</h1>
+    </Layout>
+  );
+};
+
+export default Unauthenticated;
