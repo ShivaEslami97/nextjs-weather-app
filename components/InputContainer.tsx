fd4c47b2e8790cc605a4376ee00961690af8480e
Shiva Eslami <shivaslami@gmail.com>
2023-03-13 16:17:32 +0330

    refactor: remove children from props

--- components/InputContainer.tsx
+++ components/InputContainer.tsx
@@ -1,28 +1,32 @@
 import Image from "next/image";
-import { ReactNode } from "react";
+import { forwardRef, ReactNode } from "react";
 
 type Props = {
-  children?: ReactNode;
   className?: string;
   type: string;
   placeholder?: string;
   iconAlt: string;
   iconsrc: string;
 };
+type Ref = HTMLInputElement;
 
-const InputContainer = ({
-  className,
-  type,
-  placeholder,
-  iconAlt,
-  iconsrc,
-}: Props) => {
+const InputContainer = forwardRef<Ref, Props>(function InputContainer(
+  props,
+  ref
+) {
+  const { className, iconsrc, iconAlt, placeholder, type } = props;
+
   return (
     <div className={`auth__input-container ${className}`}>
       <Image width={25} height={25} src={iconsrc} alt={iconAlt} />
-      <input type={type} className="auth__input" placeholder={placeholder} />
+      <input
+        type={type}
+        ref={ref}
+        className="auth__input"
+        placeholder={placeholder}
+      />
     </div>
   );
-};
+});
 
 export default InputContainer;
