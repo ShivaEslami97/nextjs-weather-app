16c9798afff1d5cca07e1f5800458f08762c66aa
Shiva Eslami <shivaslami@gmail.com>
2023-03-13 15:40:59 +0330

    feat: Add react-toastify for better user feedback

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import "react-toastify/dist/ReactToastify.css";
+import type { AppProps } from "next/app";
+import { ToastContainer } from "react-toastify";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({
+  Component,
+  pageProps: { ...pageProps },
+}: AppProps) {
+  return (
+    <>
+      <ToastContainer autoClose={5000} theme="colored" newestOnTop={true} />
+      <Component {...pageProps} />
+    </>
+  );
 }
