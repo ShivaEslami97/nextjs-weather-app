1f24ded17c4b27cce2bfaff96238d488abb7e8e5
Shiva Eslami <shivaslami@gmail.com>
2023-03-14 22:58:55 +0330

    feat: add login button to unauthenticated page

--- pages/unauthenticated.tsx
+++ pages/unauthenticated.tsx
@@ -1,9 +1,13 @@
 import Layout from "@/components/Layout";
+import Link from "next/link";
 
 const Unauthenticated = () => {
   return (
-    <Layout title="authenticated page">
-      <h1 className="text-lg">You should login to view this page</h1>
+    <Layout title="authenticated page" className="text-center">
+      <h1 className="text-lg">You should login to view requested page</h1>
+      <Link href="/" className="btn__secondary mx-auto">
+        Login
+      </Link>
     </Layout>
   );
 };
