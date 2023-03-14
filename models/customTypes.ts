8f7bb1d456f3644d993fd0dd6501198b7c585a71
Shiva Eslami <shivaslami@gmail.com>
2023-03-14 23:10:03 +0330

    add weather data types

--- models/customTypes.ts
+++ models/customTypes.ts
@@ -18,12 +18,17 @@ export type Password = {
   userPassword: string;
 };
 export type WeatherData = {
+  coord: {
+    lon: number;
+    lat: number;
+  };
   weather: {
     id: number;
     main: string;
     description: string;
     icon: string;
-  };
+  }[];
+  base: string;
   main: {
     temp: number;
     feels_like: number;
@@ -32,14 +37,46 @@ export type WeatherData = {
     pressure: number;
     humidity: number;
   };
+  visibility: number;
   wind: {
     speed: number;
     deg: number;
   };
+  clouds: {
+    all: number;
+  };
+  dt: number;
   sys: {
+    type: number;
+    id: number;
     country: string;
     sunrise: number;
     sunset: number;
   };
+  timezone: number;
+  id: number;
+  name: string;
+  cod: number;
+};
+
+export type Cities = {
+  name: string;
+  local_names: LocalNames[];
+  lat: number;
+  lon: number;
+  country: string;
+};
+
+export type LocalNames = {
+  ar?: string;
+  ascii?: string;
+  de?: string;
+  en?: string;
+  feature_name?: string;
+  fr?: string;
+};
+
+export type LoadedCities = {
+  country: string;
   name: string;
 };
