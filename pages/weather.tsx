cd9b8eb97b11d755fb6fdbb7c9f6e8b0d95ed7f9
Shiva Eslami <shivaslami@gmail.com>
2023-03-14 23:13:14 +0330

    feat: add data fetching to Weather component

--- pages/weather.tsx
+++ pages/weather.tsx
@@ -1,52 +1,97 @@
 import Layout from "@/components/Layout";
-import Image from "next/image";
-import searchIcon from "@/public/search_magnifier_mobile ui_zoom_icon.svg";
-import elgoli from "@/public/elgoli.svg";
-import weatherImage from "@/public/weather.svg";
-import InputContainer from "@/components/InputContainer";
 import { getSession, signOut } from "next-auth/react";
 import { NextPage } from "next";
 import { GetServerSideProps } from "next";
+import SearchBar from "@/components/SearchBar";
+import { API_KEY, BASE_URL, fetchData } from "@/lib/weather-services";
+import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import { WeatherData } from "@/models/customTypes";
+import WeatherResult from "@/components/WeatherResult";
+import { useRouter } from "next/router";
 
-const Weather: NextPage = (): JSX.Element => {
+const Weather: NextPage = () => {
+  const [currentWeather, setCurrentWeather] = useState<WeatherData | null>(
+    null
+  );
+  const [city, setCity] = useState("Tabriz");
+  const [tempIsLow, setTempIsLow] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    const getWeatherData = async () => {
+      if (city.trim().length > 0) {
+        try {
+          //Fetch weather data and update the state
+          const weather: WeatherData = await fetchData(
+            `${BASE_URL}weather?q=${city}&limit=6&appid=${API_KEY}&units=metric`
+          );
+
+          setCurrentWeather(weather);
+        } catch (error) {
+          if (error instanceof Error) {
+            toast.error(error.message || "Something went wrong!");
+          }
+        }
+      }
+    };
+    getWeatherData();
+  }, [city]);
+
+  useEffect(() => {
+    // Update the background image based on the weather condition
+    if (currentWeather) {
+      const temp = +currentWeather?.main.temp.toFixed();
+      if (temp < 3) {
+        setTempIsLow(true);
+      } else {
+        setTempIsLow(false);
+      }
+    }
+  }, [currentWeather]);
+
+  // handles the logout process by clearing the session and redirecting to the login page.
   function logoutHandler() {
-    signOut({ callbackUrl: "/" });
+    const data = signOut({ redirect: false, callbackUrl: "/" });
+    data.then((res) => {
+      // Redirect to the login page.
+      router.push(res.url);
+    });
+  }
+
+  // Determine the appropriate background image class based on the temperature.
+  const cardBg = tempIsLow ? "bg-tempLow" : "bg-tempHigh";
+
+  // A default content for when no weather data has been found yet.
+  let weatherResultContent = <p className="py-5">Nothing Found!</p>;
+  if (currentWeather) {
+    weatherResultContent = <WeatherResult weatherData={currentWeather} />;
   }
 
   return (
-    <Layout title="weather App" className="w-1/4">
-      <div className="w-full flex flex-col justify-center items-center relative z-10 text-center">
-        <InputContainer
-          className="w-full"
-          type="text"
-          placeholder="Tabriz, Iran"
-          iconAlt="search icon"
-          iconsrc={searchIcon}
-        />
-        <Image
-          src={weatherImage}
-          alt="weather image"
-          className="mx-auto"
-          width={150}
-          height={150}
-        />
-        <h2 className="weather__temp">30 °C</h2>
+    <Layout
+      title="Weather App"
+      className={`w-3/4 ${cardBg} bg-cover bg-card bg-blend-overlay lg:w-1/4`}
+    >
+      <div className="weather">
+        <SearchBar onCityChange={setCity} />
+        {weatherResultContent}
         <button
-          className="text-primary text-base mt-5 p-2 mr-auto border border-primary rounded-sm"
+          className="btn btn__secondary"
           onClick={logoutHandler}
+          type="button"
         >
           Logout
         </button>
       </div>
-      <Image src={elgoli} alt="" className="weather__bg" />
     </Layout>
   );
 };
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const session = await getSession(context);
-  console.log(session);
 
+  // enforce authentication for "weather" route.
   if (!session) {
     return {
       redirect: {
