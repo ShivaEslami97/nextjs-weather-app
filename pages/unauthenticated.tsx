31996b52558284e0af6dd7eca76cabb12da423d6
Shiva Eslami <shivaslami@gmail.com>
2023-03-15 15:03:15 +0330

    fix: center content of component

--- pages/unauthenticated.tsx
+++ pages/unauthenticated.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const Unauthenticated = () => {
   return (
-    <Layout title="authenticated page" className="text-center">
+    <Layout title="authenticated page" className="text-center justify-center">
       <h1 className="text-lg">You should login to view requested page</h1>
       <Link href="/" className="btn__secondary mx-auto">
         Login
